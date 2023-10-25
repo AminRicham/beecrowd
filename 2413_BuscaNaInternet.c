6252c8f82bad253b361f264e1cd7b73473ef1059
AminRicham <110353705+AminRicham@users.noreply.github.com>
2023-10-24 21:13:05 -0300

    Create 2413_BuscaNaInternet.c

---
+++ 2413_BuscaNaInternet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+    int num_terceiro_link;
+    scanf("%d",&num_terceiro_link);
+    
+    int num_primeiro_link = 4*num_terceiro_link;
+    printf("%d\n",num_primeiro_link);
+    
+    return 0;
+}
